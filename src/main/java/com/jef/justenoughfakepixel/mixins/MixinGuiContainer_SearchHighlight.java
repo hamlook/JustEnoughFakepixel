cc34f1eda5bea7ac04c25c7f40002104bccb7de6
megalodon-process <251696086+megalodon-process@users.noreply.github.com>
2026-03-10 12:15:35 +0530

    Update and rename MixinRenderItem_SearchHighlight.java to MixinGuiContainer_SearchHighlight.java

---
+++ src/main/java/com/jef/justenoughfakepixel/mixins/MixinGuiContainer_SearchHighlight.java
@@ -0,0 +1,19 @@
+package com.jef.justenoughfakepixel.mixins;
+
+import com.jef.justenoughfakepixel.features.misc.SearchBar;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiContainer.class)
+public class MixinGuiContainer_SearchHighlight {
+
+    @Inject(method = "drawSlot", at = @At("RETURN"))
+    public void drawSlot(Slot slot, CallbackInfo ci) {
+        if (slot == null || slot.getStack() == null) return;
+        SearchBar.renderHighlightForItem(slot.getStack(), slot.xDisplayPosition, slot.yDisplayPosition);
+    }
+}
