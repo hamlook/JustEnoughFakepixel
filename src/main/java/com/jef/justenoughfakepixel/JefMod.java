2682c8f370982805e79f7b942d7e53f7709c39c5
hamlook <hamlockgit@gmail.com>
2026-03-19 05:29:28 +0500

    1.2.3 (funny)

--- src/main/java/com/jef/justenoughfakepixel/JefMod.java
+++ src/main/java/com/jef/justenoughfakepixel/JefMod.java
@@ -47,7 +47,7 @@ public class JefMod {
 
     public static final String MODID   = "justenoughfakepixel";
     public static final String NAME    = "JustEnoughFakepixel";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.2.3";
 
     @SubscribeEvent
     public void onServerJoin(FMLNetworkEvent.ClientConnectedToServerEvent e) {
