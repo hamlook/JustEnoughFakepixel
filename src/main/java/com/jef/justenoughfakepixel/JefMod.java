c82d4d6ce1617ecdf896ec2bc8c7a23358cd2e5b
hamlook <hamlockgit@gmail.com>
2026-04-04 12:36:17 +0500

    version bump

--- src/main/java/com/jef/justenoughfakepixel/JefMod.java
+++ src/main/java/com/jef/justenoughfakepixel/JefMod.java
@@ -36,7 +36,7 @@ public class JefMod {
 
     public static final String MODID   = "justenoughfakepixel";
     public static final String NAME    = "JustEnoughFakepixel";
-    public static final String VERSION = "1.2.4";
+    public static final String VERSION = "1.2.5";
 
     public static JefConfig config;
     public static Logger logger;
--- src/main/java/com/jef/justenoughfakepixel/features/dungeons/puzzles/WaterSolver.java
+++ src/main/java/com/jef/justenoughfakepixel/features/dungeons/puzzles/WaterSolver.java
@@ -30,7 +30,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-
+@Deprecated
 public class WaterSolver {
 
     private static final Minecraft mc = Minecraft.getMinecraft();
